3792794b017dda6865324fd84d9eaebeb21b834d
petar <petar_m@mail.bg>
2017-04-04 12:40:48 +0300

    SimpleExecutor initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# executables.executors
+## Executables.Executors.Simple  
+
+A very simple implementation of IExecutor and IExecutorAsync ([https://github.com/petar-m/executables](https://github.com/petar-m/executables)).  
+  
+This is not a "real world" implementation, rather a starting point for something useful.  
---
+++ src/M.Executables.Executors.Simple/SimpleExecutor.cs
@@ -0,0 +1,69 @@
+﻿using System.Threading.Tasks;
+
+namespace M.Executables.Executors.Simple
+{
+    public class SimpleExecutor : IExecutor, IExecutorAsync
+    {
+        private readonly IInstanceProvider instanceProvider;
+
+        public SimpleExecutor(IInstanceProvider instanceProvider)
+        {
+            this.instanceProvider = instanceProvider;
+        }
+
+        public void Execute<TExecutableVoid>() where TExecutableVoid : IExecutableVoid
+        {
+            var executable = ResolveExecutable<TExecutableVoid>();
+            executable.Execute();
+        }
+
+        public void Execute<TExecutableVoid, TInput>(TInput input) where TExecutableVoid : IExecutableVoid<TInput>
+        {
+            var executable = ResolveExecutable<TExecutableVoid>();
+            executable.Execute(input);
+        }
+
+        public TResult Execute<TExecutable, TResult>() where TExecutable : IExecutable<TResult>
+        {
+            var executable = ResolveExecutable<TExecutable>();
+            return executable.Execute();
+        }
+
+        public TResult Execute<TExecutable, TInput, TResult>(TInput input) where TExecutable : IExecutable<TInput, TResult>
+        {
+            var executable = ResolveExecutable<TExecutable>();
+            return executable.Execute(input);
+        }
+
+        public async Task ExecuteAsync<TExecutableVoid>() where TExecutableVoid : IExecutableVoid
+        {
+            var executable = ResolveExecutable<TExecutableVoid>();
+            await Task.Run(() => executable.Execute());
+        }
+
+        public async Task ExecuteAsync<TExecutableVoid, TInput>(TInput input) where TExecutableVoid : IExecutableVoid<TInput>
+        {
+            var executable = ResolveExecutable<TExecutableVoid>();
+            await Task.Run(() => executable.Execute(input));
+        }
+
+        public async Task<TResult> ExecuteAsync<TExecutable, TResult>() where TExecutable : IExecutable<TResult>
+        {
+            var executable = ResolveExecutable<TExecutable>();
+            TResult result = await Task.Run(() => executable.Execute());
+            return result;
+        }
+
+        public async Task<TResult> ExecuteAsync<TExecutable, TInput, TResult>(TInput input) where TExecutable : IExecutable<TInput, TResult>
+        {
+            var executable = ResolveExecutable<TExecutable>();
+            TResult result = await Task.Run(() => executable.Execute(input));
+            return result;
+        }
+
+        private T ResolveExecutable<T>()
+        {
+            return (T)instanceProvider.GetInstance(typeof(T));
+        }
+    }
+}
