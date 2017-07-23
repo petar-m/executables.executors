14ca9d38ac60315d7d3c9214940e37006bb5a3ae
petar <petar_m@mail.bg>
2017-07-23 18:32:48 +0300

    Upgraded M.Executables to 2.0.0

--- src/M.Executables.Executors.Simple/SimpleExecutor.cs
+++ src/M.Executables.Executors.Simple/SimpleExecutor.cs
@@ -11,53 +11,53 @@ namespace M.Executables.Executors.Simple
             this.instanceProvider = instanceProvider;
         }
 
-        public void Execute<TExecutableVoid>() where TExecutableVoid : IExecutableVoid
+        public void Execute<TExecutableVoid>() where TExecutableVoid : class, IExecutableVoid
         {
             var executable = ResolveExecutable<TExecutableVoid>();
             executable.Execute();
         }
 
-        public void Execute<TExecutableVoid, TInput>(TInput input) where TExecutableVoid : IExecutableVoid<TInput>
+        public void Execute<TExecutableVoid, TInput>(TInput input) where TExecutableVoid : class, IExecutableVoid<TInput>
         {
             var executable = ResolveExecutable<TExecutableVoid>();
             executable.Execute(input);
         }
 
-        public TResult Execute<TExecutable, TResult>() where TExecutable : IExecutable<TResult>
+        public TResult Execute<TExecutable, TResult>() where TExecutable : class, IExecutable<TResult>
         {
             var executable = ResolveExecutable<TExecutable>();
             return executable.Execute();
         }
 
-        public TResult Execute<TExecutable, TInput, TResult>(TInput input) where TExecutable : IExecutable<TInput, TResult>
+        public TResult Execute<TExecutable, TInput, TResult>(TInput input) where TExecutable : class, IExecutable<TInput, TResult>
         {
             var executable = ResolveExecutable<TExecutable>();
             return executable.Execute(input);
         }
 
-        public async Task ExecuteAsync<TExecutableVoid>() where TExecutableVoid : IExecutableVoid
+        public async Task ExecuteAsync<TExecutableVoidAsync>() where TExecutableVoidAsync : class, IExecutableVoidAsync
         {
-            var executable = ResolveExecutable<TExecutableVoid>();
-            await Task.Run(() => executable.Execute());
+            var executable = ResolveExecutable<TExecutableVoidAsync>();
+            await executable.ExecuteAsync();
         }
 
-        public async Task ExecuteAsync<TExecutableVoid, TInput>(TInput input) where TExecutableVoid : IExecutableVoid<TInput>
+        public async Task ExecuteAsync<TExecutableVoidAsync, TInput>(TInput input) where TExecutableVoidAsync : class, IExecutableVoidAsync<TInput>
         {
-            var executable = ResolveExecutable<TExecutableVoid>();
-            await Task.Run(() => executable.Execute(input));
+            var executable = ResolveExecutable<TExecutableVoidAsync>();
+            await executable.ExecuteAsync(input);
         }
 
-        public async Task<TResult> ExecuteAsync<TExecutable, TResult>() where TExecutable : IExecutable<TResult>
+        public async Task<TResult> ExecuteAsync<TExecutableAsync, TResult>() where TExecutableAsync : class, IExecutableAsync<TResult>
         {
-            var executable = ResolveExecutable<TExecutable>();
-            TResult result = await Task.Run(() => executable.Execute());
+            var executable = ResolveExecutable<TExecutableAsync>();
+            TResult result = await executable.ExecuteAsync();
             return result;
         }
 
-        public async Task<TResult> ExecuteAsync<TExecutable, TInput, TResult>(TInput input) where TExecutable : IExecutable<TInput, TResult>
+        public async Task<TResult> ExecuteAsync<TExecutableAsync, TInput, TResult>(TInput input) where TExecutableAsync : class, IExecutableAsync<TInput, TResult>
         {
-            var executable = ResolveExecutable<TExecutable>();
-            TResult result = await Task.Run(() => executable.Execute(input));
+            var executable = ResolveExecutable<TExecutableAsync>();
+            TResult result = await executable.ExecuteAsync(input);
             return result;
         }
 
